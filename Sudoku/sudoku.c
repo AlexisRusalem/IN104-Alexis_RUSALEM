33c1d7fa54db9e0c93c23cf71cb3649936da4372
AlexisRusalem <alexisrusalem@outlook.fr>
2022-03-29 17:39:09 +0200

    t2

--- Sudoku/sudoku.c
+++ Sudoku/sudoku.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int area[9][9];
-
+	
+	
 int main(){
-	printf("%d\n", area[1][1]);
+	int area[9][9];
+	int i, j;
+
+	for(j=0;j<9;++j){
+		for(i=0;i<9;++i){
+			area[j][i]=(i+j*3+j/3)%9 +1;
+			}
+			}
+	for(i=0;i<9;++i)
+		{
+		for(j=0;j<9;++j){
+			printf("%d  ", area[i][j]);
+			}
+		printf("\n");
 	}
-	
-	
-test 6
+	}
+
